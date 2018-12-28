9013b6775e10915d67f31e984db34332bf911901
Carlavilla <carlavilla@mailbox.org>
2018-12-28 13:38:35 +0100

    QtEmu 2.0 release

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 {
     QApplication qtemuApp(argc, argv);
     qtemuApp.setApplicationName("QtEmu");
-    qtemuApp.setApplicationVersion("2.0-beta");
+    qtemuApp.setApplicationVersion("2.0");
     qtemuApp.setOrganizationName("QtEmu");
     qtemuApp.setOrganizationDomain("https://www.qtemu.org");
 
